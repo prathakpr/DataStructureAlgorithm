e1f04234285b41f4b14b4507defa36f4d4d422f5
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-06 12:41:31 +0530

    if else

---
+++ ifelse.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i;
+    cin>>i;
+
+    if(i>=18){
+        cout<<"you are adult";
+    }
+    else{
+        cout<<"you are not adult";
+    }
+}
--- test.cpp
+++ test.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hello";
-    return 0;
+    string s;
+    getline(cin,s);
+    cout<<s;
 }
