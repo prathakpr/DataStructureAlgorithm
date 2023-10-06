21c3cfe6b5bf8cea5f9670ea1d4673fbab8d1833
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-06 21:03:54 +0530

    Array and SStrings are added

---
+++ Array and Strings/Strings.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    string r="RajuBhai";
+    cout<<r[2];
+    cout<<"\n"<<"size of string is :"<<r.size(); 
+    cout<<"\n value at 7th which is last :"<<r[7];
+}
---
+++ Array and Strings/array.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int raju[6];
+    cin>>raju[1]>>raju[2];
+    cout<<raju[1]<<"\n"<<raju[2]<<"\n"<<raju[3]<<"\n where third one is garbage value";
+}
