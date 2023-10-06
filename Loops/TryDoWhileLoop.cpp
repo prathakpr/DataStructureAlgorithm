605474cb92840405d9ad172e23eb9ac37d0b9ba3
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-06 21:48:18 +0530

    Loops are added

---
+++ Loops/TryDoWhileLoop.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int  main(){
+    int i=11;
+    do{
+        cout<<"bhai"<<"\n";
+        i++;
+    }while(i<=10);
+    return 0;
+}
---
+++ Loops/TryWhileLoops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i=1;
+    while(i<=15){
+        cout<<i<<"\n";
+        i++;
+    }
+}
