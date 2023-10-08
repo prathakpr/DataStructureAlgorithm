3429f6b524943b8606bf87f895da12f94c493d05
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-08 14:43:30 +0530

    triangle with number

--- Patterns/print triangle number.cpp
+++ Patterns/print triangle number.cpp
@@ -4,7 +4,7 @@ using namespace std;
 void print1(int n){
     for(int i=1; i<=n; i++){
         for(int j=1; j<=i; j++){
-            cout<<i<<" ";
+            cout<<j<<" ";
         }
         cout<<"\n";
     }
