a2d8b830a1d475fe9e87a8cb527c7f534a385478
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-08 14:23:38 +0530

    phle equal nhi kra tha

--- Patterns/printTriangle.cpp
+++ Patterns/printTriangle.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 void print1(int n){
     for(int i=0; i<n; i++){
-        for(int j=0; j<i; j++){
+        for(int j=0; j<=i; j++){
             cout<<'*';
         }
         cout<<"\n";
@@ -11,12 +11,13 @@ void print1(int n){
 }
 
 int main(){
-    int t;
-    cin>>t;
-    for(int i=0; i<t; i++){
-        int n;
-        cin>>n;
-        print1(n);
-    }
+    int n;
+    cin>>n;
+    print1(n);
+    //for(int i=0; i<t; i++){
+      //  int n;
+        //cin>>n;
+        //print1(n);
+    //}
 
 }
