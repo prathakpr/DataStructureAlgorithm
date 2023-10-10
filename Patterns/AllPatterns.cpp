7e11a8e4d96c007dfed122db650ffb8e292cb10a
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-10 10:45:04 +0530

    print 5 with spaces

--- Patterns/AllPatterns.cpp
+++ Patterns/AllPatterns.cpp
@@ -131,11 +131,31 @@ void print5(int n){
         cout<<"\n";
     }
 }
- 
+void print5_with_space(int n){
+        for(int i=1; i<=2*n-1; i++){
+            //stars
+            int stars=i;
+            if(i>n){
+                stars=4*n-2*i;
+            }
+            else{
+                stars=2*i-1;
+            }
+            for(int j=1; j<=stars; j++){
+                if(j%2==0){
+                    cout<<" ";
+                }
+                else{
+                cout<<"*";
+                }
+            }
+            cout<<"\n";
+        }
+}
     int main(){
         int n;
         cin>>n;
-        print5(n);
+        print5_with_space(n);
         
         return 0;
     }
