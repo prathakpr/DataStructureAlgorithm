ada26c3ff508f75644379125a5bdeaa51644330a
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-14 10:38:58 +0530

    square pateern

--- Patterns/AllPatterns.cpp
+++ Patterns/AllPatterns.cpp
@@ -377,10 +377,28 @@ void print17(int n){
         cout<<"\n";
     }
 }    
+void print18(int n){
+    for(int i=0; i<n; i++){
+
+        for(int j=0; j<n;j++){
+            if(i==0 || i==n-1) {
+                cout<<"*";
+        }
+        else if(j==0 || j==n-1){
+            cout<<"*";
+        }
+        else {
+            cout<<" ";
+        }
+    }
+     cout<<"\n";
+    }
+}    
+    
     int main(){
         int n;
         cin>>n;
-        print17(n);
+        print18(n);
         
         return 0;
     }
