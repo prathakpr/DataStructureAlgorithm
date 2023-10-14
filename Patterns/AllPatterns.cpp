b56993ed25d17995c300d6314b6c04da8ffeedb6
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-14 11:53:20 +0530

    2d matrix pattern

--- Patterns/AllPatterns.cpp
+++ Patterns/AllPatterns.cpp
@@ -394,11 +394,23 @@ void print18(int n){
      cout<<"\n";
     }
 }    
+void print19(int n){
+    for(int i=0; i<2*n-1; i++){
+        for(int j=0; j<2*n-1; j++){
+            int top=i;
+            int left=j;
+            int right=2*(n-1)-j;
+            int down=2*(n-1)-i;
+            cout<<n-min(min(top,down),min(left,right));
+        }
+        cout<<"\n";
+    }
+}    
     
     int main(){
         int n;
         cin>>n;
-        print18(n);
+        print19(n);
         
         return 0;
     }
