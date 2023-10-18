1ce62938345e709bfc35614000e7d3d8f9dfeaf6
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-18 12:23:59 +0530

    reversing a number

--- Basic Maths/try.cpp
+++ Basic Maths/try.cpp
@@ -13,8 +13,20 @@ void count2(int n){
     int cnt=(int)log10(n)+1;
     cout<<cnt;
 }
+void reverse(int n){
+    int revn=0;
+    int lastdigit;
+    while(n>0){
+        lastdigit=n%10;
+        n=n/10;
+        revn=revn*10+lastdigit;
+    }
+    cout<<revn;
+}
+
+
 int main(){
     int n;
     cin>>n;
-    count1(n);
+    reverse(n);
 }
