5772dbaae6fe714acaa30c87968f60c32bb47c02
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-21 16:43:20 +0530

    all divisor less time complexity and sorted

--- Basic Maths/try.cpp
+++ Basic Maths/try.cpp
@@ -77,10 +77,20 @@ void alldivisor_lessTC (int n){
     }
 
 }
-
+void alldivisor_lessTC_Sorted(int n){
+    vector <int> ls;
+    for(int i=1; i<=sqrt(n); i++){
+        if(n%i==0){
+            ls.emplace_back(i);
+            if(n/i !=i) ls.emplace_back(n/i);
+        }
+    }
+    sort(ls.begin() , ls.end());
+    for(auto it:ls) cout<<it<<" ";
+}
 
 int main(){
     int n;
     cin>>n;
-    alldivisor_lessTC(n);
+    alldivisor_lessTC_Sorted(n);
 }
