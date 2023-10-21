ef74b45bdf75bf3cb979ba050ac6a33488ef6ba3
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-10-21 17:59:36 +0530

    gcd with euclidean algo

--- Basic Maths/try.cpp
+++ Basic Maths/try.cpp
@@ -108,12 +108,21 @@ void gcd(int n1, int n2){
         }
     }
 }
+void gcd_Euclidean(int n1, int n2){
+    while(n1>0 && n2>0){
+        if(n1>n2) n1=n1%n2;
+        else n2=n2%n1;
+    }
+    if(n1==0) cout<<n2;
+    else cout<<n1;
+}
+
 int main2(){
     int n1,n2;
     cin>>n1;
     cout<<"other"<<"\n";
     cin>>n2;
-    gcd(n1,n2);
+    gcd_Euclidean(n1,n2);
 }
 
 int main(){
