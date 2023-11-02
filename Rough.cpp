2c636326fa58adccec8551611c82d81e21866157
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-11-02 19:09:16 +0530

    rough added

---
+++ Rough.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int a=0, b=1;
+    cout<<"series upto :"<<n<<"\n";
+
+    if(n>=1) cout<<a;
+    if(n>=2) cout<<","<<b;
+    for(int i=3; i<=n; i++){
+        int next=a+b;
+        cout<<","<<next;
+        a=b;
+        b=next;
+    }
+    cout<<"\n";
+}
