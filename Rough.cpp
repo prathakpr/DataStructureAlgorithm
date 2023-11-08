24b315dc82ff83dcde2fd803e3d4858628775b93
Pulkit-Rathore <73521836+Pulkit-Rathore@users.noreply.github.com>
2023-11-08 22:22:36 +0530

    rough

--- Rough.cpp
+++ Rough.cpp
@@ -1,26 +1,23 @@
+//insertion sort
+
 #include <bits\stdc++.h>
 using namespace std;
 
 int main(){
-    //selection sort
     int n;
     cin>>n;
     int arr[n];
-    for(int i=0; i<n;i++){
+    for(int i=0;i<n;i++){
         cin>>arr[i];
     }
-    //sorting here
-    for(int i=0;i<n-1;i++){
-       int mini=i;
-        for(int j=i+1;j<=n-1;j++){
-            if(arr[j]<arr[mini]) mini=j;
+
+    for(int i=1;i<n;i++){
+        int j=i;
+        while(j>0 && arr[j-1]>arr[j]){
+            swap(arr[j],arr[j-1]);
+            j--;
         }
-
-        //swaping
-        swap(arr[mini],arr[i]);
     }
-
-    //printing
     for(int i=0;i<n;i++){
         cout<<arr[i]<<",";
     }
