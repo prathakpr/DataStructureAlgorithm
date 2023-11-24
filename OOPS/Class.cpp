080fd8d2e7251f54834ceb7133f176a14dc5207c
Pulkit-Rathore <pulkitrathore770@gmail.com>
2023-11-24 15:46:04 +0530

    c

--- OOPS/Class.cpp
+++ OOPS/Class.cpp
@@ -2,14 +2,33 @@
 using namespace std;
 
 class hero {
-    public:
+
     int health=100;
+    string name;
+    int position;
+    string level;
+
+    public :
+    int getHealth(){
+        return health;
+    }
+    string getName(){
+        return name;
+    }
+
+    void setName(string n){
+        name=n;
+    }
+
+
 };
 
 int main(){
     //cration of object
     hero obj1;
     cout<<sizeof(obj1)<<endl;
-    cout<< obj1.health;
+    cout<< obj1.getHealth();
+    obj1.setName("Ramesh");
+    cout<<endl<<obj1.getName();
 
 }
