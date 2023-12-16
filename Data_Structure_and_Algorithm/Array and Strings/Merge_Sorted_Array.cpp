7ded482ee5456d0ec39f67ee9d098cb76757c3e1
Pulkit-Rathore <pulkitrathore770@gmail.com>
2023-12-16 15:20:41 +0530

    written code of merge_sorted_array

---
+++ Data_Structure_and_Algorithm/Array and Strings/Merge_Sorted_Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr1[5]={1,2,3,4,5};
+    int arr2[6]={5,6,7,8,9,10};
+
+    int size1= sizeof(arr1)/sizeof(arr1[0]);
+    int size2= sizeof(arr2)/sizeof(arr2[0]);
+
+    int new_arr[size1+size2];
+    for(int i=0; i<size1+size2;i++){
+        if(i<size1) new_arr[i]=arr1[i];
+        else new_arr[i]=arr2[size2-1-i];
+    }
+
+    for(int i=0; i<size1+size2;i++){
+        cout<<new_arr[i]<<", ";
+    }
+}
