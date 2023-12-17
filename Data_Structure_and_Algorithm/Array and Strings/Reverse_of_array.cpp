4c7d21268060dff4bbdc9d2dd787348c3fda289b
Pulkit-Rathore <pulkitrathore770@gmail.com>
2023-12-17 15:35:42 +0530

    New way reversing array

--- Data_Structure_and_Algorithm/Array and Strings/Reverse_of_array.cpp
+++ Data_Structure_and_Algorithm/Array and Strings/Reverse_of_array.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main(){
     int arr[10]={1,2,3,4,5,6,7,8,9,10};
+    int arr2[5]={5,8,7,6,9};
     for(int i=0; i<10/2; i++){
         swap(arr[i],arr[10-i-1]);
     }
@@ -10,6 +11,16 @@ int main(){
      for(int i=0; i<10; i++){
        cout<<arr[i]<<" ";
     }
+    cout<<endl<<"VV other niche VV"<<endl;
     
-    
+    // second array but other way
+
+    int s=0;
+    int e=5-1;
+    while(s<e){
+        swap(arr2[s++],arr2[e--]);
+    }
+      for(int i=0; i<=5-1; i++){
+       cout<<arr2[i]<<" ";
+    }
 }
