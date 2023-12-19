a4fdee535ae23a1b5d4d955b82939f403193722a
Pulkit-Rathore <pulkitrathore770@gmail.com>
2023-12-19 09:47:44 +0530

    added reverse word string

--- Data_Structure_and_Algorithm/Array and Strings/Reverse_Words_in_a_String_2.cpp
+++ Data_Structure_and_Algorithm/Array and Strings/Reverse_Words_in_a_String_2.cpp
@@ -1,1 +1,16 @@
-#
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string x="The Sky Is";
+    int s=0;
+    int e=x.length()-1;
+    while(s<=e){
+     swap(x[s++],x[e--]);
+    }
+
+    for(int i=0;i<x.length();i++){
+        cout<<x[i]<<" ";
+    }
+}
