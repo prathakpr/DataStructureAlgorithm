149b2c486159e92f03be7d1d8a641b891ea77e07
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-01-25 17:45:55 +0530

    inserting & craetion on BST

--- Data_Structure_and_Algorithm/Binary_Seach_Tree/Insert_Node.cpp
+++ Data_Structure_and_Algorithm/Binary_Seach_Tree/Insert_Node.cpp
@@ -1,6 +1,49 @@
 #include<iostream>
 using namespace std;
 
+class node{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d){
+        this->data =d;
+        this->left=NULL;
+        this->right=NULL;
+    }
+};
+
+node* insertInto_BST(node* root, int d){
+    //base case
+    if(root==NULL){
+        root = new node(d);
+        return root;
+    }
+    // righ part mai
+    if(d > root->data){
+        root->right=insertInto_BST(root->right,d);
+    }
+    // left part mai
+    else {
+        root->left = insertInto_BST(root->left,d);
+    }
+    return root;
+}
+void TakeInput(node* &root){
+    int data;
+    cin>>data;
+    while(data != -1){
+        root = insertInto_BST(root,data);
+        cin>>data;
+    }
+}
+
 int main(){
-    
+    node* root=NULL;
+    cout<<"enter data to insert or create bst"<<endl;
+    TakeInput(root);
+
+    return 0;
+
 }
