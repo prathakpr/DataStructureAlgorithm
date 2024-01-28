fa2d1e62fcea07de3fde217345866b0a97ddece4
Pulkit-Rathore <pulkitrathore770@gmail.com>
2024-01-28 11:33:06 +0530

    polymorphism

---
+++ OOPS/Polymorphism.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class function_overloading{
+    public:
+    void check(){
+        cout<<"no aarguments";
+    }
+    void check(int i){
+        cout<<"with argument";
+    }
+};
+
+class operator_overloading{
+    
+}
+
+int main(){
+    function_overloading obj;
+    obj.check();
+    cout<<endl;
+    obj.check(1);
+}
